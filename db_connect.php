9fd9e318a3eb833fff0b821d9d884cce938a8348
Aamir Bashir <ksaak33@gmail.com>
2025-08-03 20:04:27 +0530

    Remove commented debug code from database connection script

--- db_connect.php
+++ db_connect.php
@@ -6,16 +6,15 @@ $pass = '22647852';
 $port = '5432'; // default PostgreSQL port
 
 $_SESSION['connect'] = '';
-// phpinfo();
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass");
-// var_dump($con);
-// $_SESSION['con'] = $con;
+
 if (!$con) {
 	$_SESSION['connect'] = "Connection failed: " . pg_last_error();
 } else {
 	// $_SESSION['connect'] = "Connected successfully!";
 }
 
+// phpinfo();
 // try {
 // 	$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
 // 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
